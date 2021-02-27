fafd668b93caddbc9e2d321056bb0d231b7cd9ad
Vladislav <mestr3z@yandex.ru>
2021-02-27 23:01:37 +0300

    commit 1

---
+++ index.js
@@ -0,0 +1,38 @@
+const popupEdit = document.querySelector(".form");
+const buttonEdit = document.querySelector(".profile__edit-button");
+const popupCloseButton = document.querySelector(".form__pic");
+const buttonLike = document.querySelector('.cards__like');
+const formEdit = document.querySelector('.form__list');
+const jobInput = document.querySelector('input[name="job"]');
+const nameInput = document.querySelector('input[name="name"]');
+const userName = document.querySelector('.profile__name');
+const userJob = document.querySelector('.profile__status');
+const saveButtun = document.querySelector('.form__save-button');
+
+const popupEditOpen = () => {
+    popupEdit.classList.add('form__active');
+};
+
+const popupEditClose = () => {
+    popupEdit.classList.toggle('form__active');
+};
+
+const formEditSubmitHandler = (evt) => {
+    evt.preventDefault();
+    const jobValue = jobInput.value;
+    const nameValue = nameInput.value;
+    userName.textContent = nameValue;
+    userJob.textContent = jobValue;
+    popupEditClose();
+}
+
+
+const toggleLike = (evt) => {
+    evt.target.classList.toggle('cards__like_active');
+};
+
+
+buttonLike.addEventListener('click', toggleLike);
+buttonEdit.addEventListener('click', popupEditOpen);
+popupCloseButton.addEventListener('click', popupEditClose);
+formEdit.addEventListener('submit', formEditSubmitHandler);
